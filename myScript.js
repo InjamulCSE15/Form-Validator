8d0608e619009a621bbc89d6dcd47b2e3eebd3e2
unknown <yeasin.mohammod018@gmail.com>
2021-08-07 02:28:32 +0600

    bug created

--- myScript.js
+++ myScript.js
@@ -4,7 +4,23 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+//Show input error message:
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+  }
+
+// Event listeners: 
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    console.log("Submit button clicked!");
+    console.log(username.value);
+
+    if(username.value === ''){
+        showError();
+    }
+    else {
+        showSuccess(username);
+    }
 });
