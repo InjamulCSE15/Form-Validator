9209d9b9758b2af04821a5339356ff2a8e43b76a
unknown <yeasin.mohammod018@gmail.com>
2021-08-10 13:34:29 +0600

    one bug fixed

--- myScript.js
+++ myScript.js
@@ -4,7 +4,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-//Show input error message:
+// Show input error message:
 function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
@@ -12,15 +12,35 @@ function showError(input, message) {
     small.innerText = message;
   }
 
+// Show success outline:
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
 // Event listeners: 
 form.addEventListener('submit', function(e){
     e.preventDefault();
     console.log(username.value);
 
     if(username.value === ''){
-        showError();
+        showError(username, 'User name must be required');
     }
     else {
         showSuccess(username);
+    }
+    
+    if(email.value === ''){
+        showError(email, 'Email must be required');
+    }
+    else {
+        showSuccess(email);
+    }
+    
+    if(password.value === ''){
+        showError(password, 'Password must be required');
+    }
+    else {
+        showSuccess(password);
     }
 });
